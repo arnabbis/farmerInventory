7a5caa56c64e13042fc4f5d149b5ea6337c6016d
arnabbis <biswas.anuska.2004@gamil.com>
2024-07-19 15:48:47 +0530

    feat: added api for getting details of the invoice

--- farmer/src/main/java/com/farmerManagement/farmer/controller/invoiceController.java
+++ farmer/src/main/java/com/farmerManagement/farmer/controller/invoiceController.java
@@ -13,9 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 @RequestMapping("/invoice")
@@ -100,5 +98,43 @@ public class invoiceController {
         }
         response<invoiceEntity> response = new response<>(HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND.getReasonPhrase(), null);
         return new ResponseEntity<>(response,HttpStatus.OK);
+    }
+
+    @GetMapping("/getDetailsInInvoice")
+    public ResponseEntity<response<Map<String, Object>>> getDetailsInInvoice(@RequestParam int id) {
+        Optional<invoiceEntity> findById = invoiceservice.getInvoiceById(id);
+        if (findById.isEmpty()) {
+            response<Map<String, Object>> response = new response<>(HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND.getReasonPhrase(), null);
+            return new ResponseEntity<>(response,HttpStatus.OK);
+        }
+        invoiceEntity invoice = findById.get();
+        Optional<sellersEntity> findSeller = sellerservice.findSellerById(findById.get().getSellerId());
+        sellersEntity seller = findSeller.get();
+        Optional<purchaseOrder> findPurchaseOrder = purchaseorderservice.findById(invoice.getPurchaseId());
+        purchaseOrder purchaseOrder = findPurchaseOrder.get();
+        List<orderDetailsEntity> findAllOrderBy = orderdetailsservice.findByPurchaseOrderId(findPurchaseOrder.get().getId());
+        Map<String,Object> sellerData = new HashMap<>();
+        sellerData.put("sellerName",seller.getSellerName());
+        sellerData.put("sellerAddress",seller.getSellerAddress());
+        sellerData.put("sellerAddress",seller.getSellerAddress());
+        sellerData.put("sellerContectDetails",seller.getSellerMobileNumber());
+        Map<String,Object> purchaseData = new HashMap<>();
+        purchaseData.put("contactInfo",purchaseOrder.getContactInfo());
+        purchaseData.put("shippingAddress",purchaseOrder.getShippingAddress());
+        purchaseData.put("purchaseDate",purchaseOrder.getOrderDate());
+        purchaseData.put("orderDetails", findAllOrderBy);
+        purchaseData.put("sellerContectDetails",seller.getSellerMobileNumber());
+        Map<String, Object> invoiceData = new HashMap<>();
+        invoiceData.put("sellerId", sellerData);
+        invoiceData.put("purchaseId", purchaseData);
+        invoiceData.put("totalPrice", invoice.getTotalPrice());
+        invoiceData.put("totalQuantity", invoice.getTotalQuantity());
+        invoiceData.put("invoiceDate", invoice.getInvoiceDate());
+
+        Map<String, Object> responseData = new HashMap<>();
+        responseData.put("invoice", invoiceData);
+
+        response<Map<String, Object>> response = new response<>(HttpStatus.OK.value(), "Invoice and Seller details", responseData);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
